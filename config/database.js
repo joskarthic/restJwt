bcce86ab230973724ac26d797c9d7fd965e3b895
Karthik <karthik.bala@sterlingadministration.com>
2019-04-19 17:11:30 +0530

    db changes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 //Set up mongoose connection
 console.log('in db config');
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
+// const mongoDB = 'mongodb://localhost/node_rest_api';
+const mongoDB = 'mongodb+srv://curd_tester:curd_tester@curdtest-5xogh.mongodb.net/test?retryWrites=true';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 
